1be6343043fa2e69a8045ef0aad81d99158dcb5b
Massimo Santini <santini@dsi.unimi.it>
2012-06-13 00:53:05 +0200

    Renaming stuff
    --HG--
    rename : lw/app/__init__.py => lw/lwf/__init__.py
    rename : lw/app/assets.py => lw/lwf/assets.py
    rename : lw/app/templates/base.html => lw/lwf/templates/base.html
    rename : lw/app/templates/index.html => lw/lwf/templates/index.html

--- lw/lwf/__init__.py
+++ lw/lwf/__init__.py
@@ -3,7 +3,7 @@ from os import getcwd
 from os.path import join
 
 BASE_PATH = join( getcwd(), sys.argv[ 0 ] )
-sys.path.append( join( BASE_PATH, 'libs' ) )
+sys.path.append( join( BASE_PATH, 'vendorized' ) )
 
 from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO
 
@@ -15,13 +15,21 @@ LOGGER.setLevel( DEBUG )
 LOGGER.addHandler( handler )
 
 from flask import Flask, render_template
-from jinja2 import PackageLoader
+from jinja2 import PackageLoader, ChoiceLoader
 
 from .assets import assets
 
+from .apps import APPLICATIONS
+
 app = Flask( __name__ )
-app.jinja_loader = PackageLoader( __name__ ) # to find templates also if launched from zip
+
 app.add_url_rule( '/assets/<path:path>', 'assets', assets ) # to serve assets from zip / filesystem
+
+loaders = [ PackageLoader( __name__ ) ] 
+for prefix, blueprint in APPLICATIONS.items():
+	app.register_blueprint( blueprint, url_prefix = '/{0}'.format( prefix ) )
+	loaders.append( PackageLoader( 'lwf.apps.{0}'.format( prefix ) ) ) 
+app.jinja_loader = ChoiceLoader( loaders ) # to find templates also if launched from zip
 
 @app.route('/')
 def index():
--- lw/lwf/assets.py
+++ lw/lwf/assets.py
@@ -1,25 +1,25 @@
 from logging import getLogger
 from os.path import join, exists
-from zipfile import ZipFile
+from StringIO import StringIO
 
 from flask import send_file
 from werkzeug.exceptions import NotFound
 
 from . import BASE_PATH
+from .zipio import load
 
 LOGGER = getLogger( __name__ )
 
 if BASE_PATH.endswith( '.zip' ):
     
-    LOGGER.info( 'Serving assets from zip file {0}'.format( BASE_PATH ) )
-    def assets( path ):
-        with ZipFile( BASE_PATH, 'r' ) as zf:
-            try:
-                x = zf.open( join( 'assets', path ), 'r' )
-            except KeyError:
-                raise NotFound
-            x.name = None
-            return send_file( x, attachment_filename = path )
+	LOGGER.info( 'Serving assets from zip file {0}'.format( BASE_PATH ) )
+	ASSETS = load( BASE_PATH, lambda entry: entry.startswith( 'assets/' ) )
+	def assets( path ):
+		try:
+			content = ASSETS[ path ]
+		except KeyError:
+			raise NotFound
+		return send_file( StringIO( content ), attachment_filename = path )
 else:
 
     BASE_DIR = join( BASE_PATH, 'assets' )
