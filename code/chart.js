c4199d694e9632554aaa619cb41f553bbb4b84dd
Massimo Santini <santini@dsi.unimi.it>
2012-06-14 15:37:56 +0200

    Tweaking apps

--- code/chart.js
+++ code/chart.js
@@ -1,9 +1,5 @@
-function init() {
-	input_ints( 1, 'Numero di punti' );
-}
-
 function main( input ) {
 	var data = table( 'x', [ 'retta', 'parabola' ] );
-	for ( var i = 0; i < input[ 0 ]; i++ ) data.addRow( [ "" + i, i, i * i ] );
+	for ( var i = 0; i < input.n; i++ ) data.addRow( [ "" + i, i, i * i ] );
 	draw( data );
 }
