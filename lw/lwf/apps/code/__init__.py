f0902fc6836e997e8449b9286c9072697ddc26f9
Massimo Santini <santini@dsi.unimi.it>
2012-06-14 12:10:54 +0200

    Migrating to new input/output management

--- lw/lwf/apps/code/__init__.py
+++ lw/lwf/apps/code/__init__.py
@@ -1,15 +1,22 @@
+from itertools import groupby
+
 from flask import Blueprint, render_template, request
+from werkzeug.exceptions import NotFound
 
 from lwf import RESOURCES_PATH
-from lwf.resources import Resources
+from ...resources import Resources
+from ...ycfg import read_configs
 
 RESOURCES = Resources( RESOURCES_PATH )
+
+APPLICATIONS_LIST = read_configs( RESOURCES.load( 'applications.yaml' ), '' )
+APPLICATIONS = dict( ( ( _.name, _ ) for _ in APPLICATIONS_LIST ) )
 
 code = Blueprint( 'code', __name__ )
 
 @code.route( '/' )
 def list():
-	return render_template( 'list.html', entries = RESOURCES.entries() )
+	return render_template( 'list.html', applications = APPLICATIONS.values() )
 
 @code.route( '/load/<path:path>' )
 def load( path ):
@@ -23,6 +30,10 @@ def save( path ):
 def edit( path ):
 	return render_template( 'edit.html', path = path )
 	
-@code.route( '/run/<path:path>' )
-def run( path ):
-	return render_template( 'run.html', path = path )
+@code.route( '/run/<name>' )
+def run( name ):
+	try:
+		app = APPLICATIONS[ name ]
+	except KeyError:
+		raise NotFound
+	return render_template( 'run.html', app = app )
