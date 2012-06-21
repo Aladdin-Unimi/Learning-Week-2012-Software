834057028518df53c9b736c48b2d1aa4079c42fd
Massimo Santini <santini@dsi.unimi.it>
2012-06-21 23:58:49 +0200

    Grom yaml to xml for app configurations

--- lwapp/lwf/apps/usr/__init__.py
+++ lwapp/lwf/apps/usr/__init__.py
@@ -15,23 +15,40 @@
 # You should have received a copy of the GNU General Public License along with
 # Learning-Week-2012-Software If not, see <http://www.gnu.org/licenses/>.
 
-from itertools import groupby
+from xml.dom.minidom import parseString, Node
 
 from flask import Blueprint, render_template, request, make_response
 from werkzeug.exceptions import NotFound
 
 from lwf import USER_APPS
 from lwf.resources import Resources
-from lwf.ycfg import read_configs
 
-APPLICATIONS_LIST = read_configs( USER_APPS.load( 'applications.yaml' ), '' )
-APPLICATIONS = dict( ( ( _.name, _ ) for _ in APPLICATIONS_LIST ) )
+xml = parseString( USER_APPS.load( 'applications.xml' ) )
+
+def g0fcd( e, tag ):
+	t = e.getElementsByTagName( tag )
+	if t: return ( tag, t[ 0 ].firstChild.data )
+	else: return None
+
+USER_APPS_DESC = []
+for g in xml.getElementsByTagName( 'group' ):
+	group_name = g0fcd( g, 'name' )[ 1 ]
+	apps = []
+	for a in g.getElementsByTagName( 'application' ):
+		app = dict( ( g0fcd( a, 'name' ), ) )
+		app.update( { 
+			'code': [ c.firstChild.data for c in a.getElementsByTagName( 'code' ) ],
+			'output': [ o.firstChild.data for o in a.getElementsByTagName( 'output' ) ],
+			'input': [ dict( filter( None, ( g0fcd( i, t ) for t in ( 'name', 'type', 'desc', 'hint' ) ) ) ) for i in a.getElementsByTagName( 'input' ) ]
+		} )
+		apps.append( app )
+	USER_APPS_DESC.append( ( group_name, apps ) )
 
 usr = Blueprint( 'usr', __name__ )
 
 @usr.route( '/' )
 def list():
-	return render_template( 'list.html', applications = APPLICATIONS.values() )
+	return render_template( 'list.html', applications = USER_APPS_DESC )
 
 @usr.route( '/serve/<path:path>' )
 def serve( path ):
@@ -53,10 +70,11 @@ def save( path ):
 def edit( path ):
 	return render_template( 'edit.html', path = path )
 	
-@usr.route( '/run/<name>' )
-def run( name ):
+@usr.route( '/run/<int:group>/<int:application>' )
+def run( group, application ):
 	try:
-		app = APPLICATIONS[ name ]
+		app = USER_APPS_DESC[ group ][ 1 ][ application ]
+		print "APP", app
 	except KeyError:
 		raise NotFound
 	return render_template( 'run.html', app = app )
