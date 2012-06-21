d088c6ccd50bdb7919a74298c5b020bd765d9192
Massimo Santini <santini@dsi.unimi.it>
2012-06-21 23:57:40 +0200

    Before being deleted

--- lwapp/lwf/ycfg.py
+++ lwapp/lwf/ycfg.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License along with
 # Learning-Week-2012-Software If not, see <http://www.gnu.org/licenses/>.
 
-import yaml
+from xml.dom.minidom import parseString, Node
 
 class Dao( object ):
 	
@@ -35,11 +35,38 @@ class Dao( object ):
 
 	@staticmethod
 	def daoize( data, default = None ):
-		if isinstance( data, dict ):
-			return Dao( data, default )
+		if isinstance( data, unicode ):
+			return data
+		elif isinstance( data, dict ):
+			return Dao( dict( ( k, Dao.daoize( v, default ) ) for k, v in data.items() ), default )
 		elif isinstance( data, list ):
 			return [ Dao.daoize( _, default ) for _ in data ]
 
-def read_configs( yaml_string, default = None ):
-	return Dao.daoize( yaml.load( yaml_string ), default )
+def todict( e ):
+	def traverse( e ):
+		if e.nodeType == Node.TEXT_NODE:
+			d = e.data.strip()
+			if d: return d
+			else: return None
+		if e.nodeType != Node.ELEMENT_NODE and e.nodeType != Node.DOCUMENT_NODE: return None
+		res = []
+		for c in e.childNodes:
+			s = traverse( c )
+			if s: res.append( s )
+		if len( res ) == 1 and isinstance( res[ 0 ], unicode ):
+			return ( e.nodeName, res[ 0 ] )
+		else:
+			dres = dict()
+			for k, v in res:
+				if k in dres:
+					if isinstance( dres[ k ], list ): dres[ k ].append( v )
+					else: dres[ k ] = [ dres[ k ], v ]
+				else:
+					dres[ k ] = v
+			return ( e.nodeName, dres )
+	e.normalize()
+	return dict( ( traverse( e.firstChild ), ) )
+
+def read_configs( xml_string, default = None ):
+	return Dao.daoize( todict( parseString( xml_string ) ), default )
 
