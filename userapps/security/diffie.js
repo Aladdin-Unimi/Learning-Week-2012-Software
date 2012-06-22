83fd5e3581b207c6a944fe366e6f3653c10d4c1d
Mattia Monga <monga@debian.org>
2012-06-22 16:31:11 +0200

    Correzioni

--- userapps/security/diffie.js
+++ userapps/security/diffie.js
@@ -1,8 +1,12 @@
 function main( input ) {
-	output( "Secondo Alice, il segreto condiviso è:" 
+	output( "Secondo Alice, il numero da usare è:" 
 	    + diffie(input.n, input.p, input.x) );
-	output( "Secondo Bruno, il segreto condiviso è:" 
+	output( "Secondo Bruno, il numero da usare è:" 
 	    + diffie(input.n, input.p, input.y) );
+
+	output( "Il numero segreto condiviso è (secondo A):....");
+	output( "Il numero segreto condiviso è (secondo B):....");
+
 }
 
 function diffie( n, p, r ){
